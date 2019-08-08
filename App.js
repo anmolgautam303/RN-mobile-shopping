da99ab09ef11e8dfb23484c4cade350ff8069f88
Anmol Gautam <anmol.gautam303@gmail.com>
2019-08-08 15:18:11 +0300

    redux persist added

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-
-import store from './src/store';
-import Main from './src';
+import React from "react";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./src/store";
+import Main from "./src";
 
 const App = () => {
   return (
     <Provider store={store}>
-      <Main />
+      <PersistGate loading={null} persistor={persistor}>
+        <Main />
+      </PersistGate>
     </Provider>
   );
 };
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,17 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import reducers from '../reducers';
+import { AsyncStorage } from "react-native";
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import thunk from "redux-thunk";
+import rootReducer from "../reducers";
 
-export default createStore(reducers, applyMiddleware(thunk));
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage
+};
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = createStore(persistedReducer, applyMiddleware(thunk, logger));
+
+const persistor = persistStore(store);
+
+export { store, persistor };
