7f9e9b30bca9de2be92ae3375fad22361ad15ed3
Anmol Gautam <anmol.gautam303@gmail.com>
2019-08-08 15:23:09 +0300

    react-native-community/async-storage package added

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,4 @@
-import { AsyncStorage } from "react-native";
+import AsyncStorage from '@react-native-community/async-storage';
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
